62f5a45b52363a3c68eba2ed13567b11c9362004
rajohns <rajohns@bcbsms.com>
2015-02-11 15:18:22 -0600

    initial commit

--- RefreshControlInUIViewController/ViewController.m
+++ RefreshControlInUIViewController/ViewController.m
@@ -16,12 +16,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UITableViewController *tableViewController = [[UITableViewController alloc] init];
+    tableViewController.tableView = self.tableView;
+    
+    UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
+    [refreshControl addTarget:self action:@selector(refresh:) forControlEvents:UIControlEventValueChanged];
+    tableViewController.refreshControl = refreshControl;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"CellId"];
+    cell.textLabel.text = [NSString stringWithFormat:@"Row %ld", indexPath.row];
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20 ;
+}
+
+- (void)refresh:(UIRefreshControl *)refreshControl {
+    double delayInSeconds = 4.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    
+    [refreshControl beginRefreshing];
+    dispatch_after(popTime, dispatch_get_main_queue(), ^{
+        [refreshControl endRefreshing];
+    });
 }
 
 @end
