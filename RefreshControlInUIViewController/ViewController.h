844263a1c3cfe50fcb860e4ca702bf01f812215c
rajohns <rajohns@bcbsms.com>
2015-02-11 14:43:15 -0600

    Initial Commit

---
+++ RefreshControlInUIViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RefreshControlInUIViewController
+//
+//  Created by Adam Johns on 2/11/15.
+//  Copyright (c) 2015 Adam Johns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RefreshControlInUIViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RefreshControlInUIViewController
+//
+//  Created by Adam Johns on 2/11/15.
+//  Copyright (c) 2015 Adam Johns. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
